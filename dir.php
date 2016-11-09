0e69b3a938b57543b9b15b989249e48fd21f17f6
Egist <maysrp@gmail.com>
2016-11-09 20:19:25 +0800

    对所在目录上一目录禁止访问

--- dir.php
+++ dir.php
@@ -16,23 +16,23 @@
 <?php
 
 	$dir=$_GET['dir']?$_GET['dir']:".";
-	dirpath($dir);
+	$jugg=dirpath($dir);
+	if($jugg){
+		echo $jugg;
+	}
 	function dirpath($dir){
 		$host=$_SERVER['HTTP_HOST'];
 		$primary=strstr($_SERVER['SCRIPT_NAME'],basename(__FILE__),TRUE);
 		$odir="./".$dir;
 		$dir_array=scandir($odir);
+		$str="";
 		foreach ($dir_array as $key => $value) {
-			echo "<tr>";
+			$str=$str."<tr>";
 			$path=$odir."/".$value;
-			//$bm=mb_detect_encoding($value);
-			//echo $bm;
-			//$value=iconv("GBK","UTF-8",$value);
 			if(is_dir($path)){
-				echo "<td><span class=\"glyphicon glyphicon-folder-close\"></span><a href=?dir=".$dir."/".$value."> ".$value."</a></td>";
-				echo "<td>目录</td>";
+				$str=$str."<td><span class=\"glyphicon glyphicon-folder-close\"></span><a href=\"?dir=".$dir."/".$value."\"> ".$value."</a></td><td>目录</td>";
 			}else{
-				echo "<td>";
+				$str=$str."<td>";
 				$ex=strtolower(substr(strrchr($value, '.'), 1));
 				switch ($ex) {
 					case 'jpg':
@@ -41,7 +41,7 @@
 					case 'ico':
 					case 'png':
 					case 'bmp':
-						echo "<span class=\"glyphicon glyphicon-picture\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-picture\"></span>";
 						break;
 					case 'mp4':
 					case 'avi':
@@ -51,14 +51,14 @@
 					case 'rmvb':
 					case 'wmv':
 					case 'rm':
-						echo "<span class=\"glyphicon glyphicon-film\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-film\"></span>";
 						break;
 					case 'zip':
 					case 'rar':
 					case '7z':
 					case 'gz':
 					case 'tgz':
-						echo "<span class=\"glyphicon glyphicon-tasks\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-tasks\"></span>";
 						break;
 					case 'pdf':
 					case 'doc';
@@ -67,7 +67,7 @@
 					case 'docx';
 					case 'ppt';
 					case 'pptx';
-						echo "<span class=\"glyphicon glyphicon-book\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-book\"></span>";
 						break;
 					case 'php':
 					case 'py':
@@ -77,30 +77,39 @@
 					case 'html':
 					case 'ini':
 					case 'json':
-						echo "<span class=\"glyphicon glyphicon-file\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-file\"></span>";
 						break;
 					case 'exe':
 					case 'bat':
 					case 'dll':
-						echo "<span class=\"glyphicon glyphicon-th-large\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-th-large\"></span>";
 						break;
 					case 'mp3':
 					case 'wav':
 					case 'flv':
-						echo "<span class=\"glyphicon glyphicon-music\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-music\"></span>";
 						break;
 					case 'torrent':
-						echo "<span class=\"glyphicon glyphicon-magnet\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-magnet\"></span>";
 						break;
 					default:
-						echo "<span class=\"glyphicon glyphicon-stop\"></span>";
+						$str=$str."<span class=\"glyphicon glyphicon-stop\"></span>";
 						break;
 				}
-				echo "<a href=".$dir."/".$value."> ".$value."</a></td>";
-				echo  "<td>".filesize($path)."B</td>";
+				$str=$str."<a href=\"".$dir."/".$value."\"> ".$value."</a></td>";
+				$str=$str."<td>".filesize($path)."B</td>";
 			}		
-			echo "</tr>";
+			$str=$str."</tr>";
 		}
+		chdir($odir);
+		$or=strlen(dirname(__FILE__));
+		$now=strlen(getcwd());
+		if($or>$now){
+			return false;
+		}else{
+			return $str;
+		}
+
 	}
 ?>
 		</table>
@@ -110,3 +119,6 @@
 	</div>	
 	
 </body>
+
+
+
